d88682f39f8b75e610798410e91c50ff526e3d00
Alex Ponting <happy.ajp050789@gmail.com>
2014-02-08 22:21:16 -0800

    Prior to shenanigans, we save progress

--- src/genartis/agent.clj
+++ src/genartis/agent.clj
@@ -1,4 +1,5 @@
 (ns genartis.agent
+  (:require [quil.core :refer (red green blue)])
   (:use [genartis.geometry :only [make-triangle]]))
 
 (def TRI-COUNT 50)
@@ -17,6 +18,19 @@
 (defn mutate-painting [p]
   (map (fn [tri] (if (< (rand) TRI-MUTATION-RATE)) (mutate-tri tri) tri) p))
 
+(defn abs [x]
+  (if (pos? x) x (- x)))
+
 ;; Determining painting fitness
-(defn painting-fitness [p]
-  (rand))
+(defn painting-fitness [goal p]
+  (reduce +
+    (map 
+      (fn [o n]
+        (let [[o-r o-g o-b] [(red o) (green o) (blue o)]
+              [n-r n-g n-b] [(red n) (green n) (blue n)]
+              r-r           (- 255 (abs (- o-r n-r)))
+              r-g           (- 255 (abs (- o-g n-g)))
+              r-b           (- 255 (abs (- o-b n-b)))
+              final         (+ r-r r-g r-b)]
+          final))
+      goal p)))
